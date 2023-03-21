756f27c640e2fac6bc8121ad64301da25fe67938
Loïc Mathieu <loikeseke@gmail.com>
2023-03-21 14:36:00 +0100

    feat: plugin categories and icons

---
+++ src/main/java/io/kestra/plugin/powerbi/package-info.java
@@ -0,0 +1,7 @@
+@PluginSubGroup(
+    description = "This sub-group of plugins contains tasks for using Microsoft Power BI.",
+    categories = PluginSubGroup.PluginCategory.MISC
+)
+package io.kestra.plugin.powerbi;
+
+import io.kestra.core.models.annotations.PluginSubGroup;
