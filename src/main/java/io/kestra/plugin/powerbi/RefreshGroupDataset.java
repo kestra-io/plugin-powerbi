c132bace74adb59f1617c83244658773f6956938
Loïc Mathieu <loikeseke@gmail.com>
2024-01-03 16:59:22 +0100

    feat: Micronaut 4

--- src/main/java/io/kestra/plugin/powerbi/RefreshGroupDataset.java
+++ src/main/java/io/kestra/plugin/powerbi/RefreshGroupDataset.java
@@ -20,7 +20,7 @@ import java.util.Locale;
 import java.util.Map;
 import java.util.Optional;
 
-import javax.validation.constraints.NotNull;
+import jakarta.validation.constraints.NotNull;
 
 import static io.kestra.core.utils.Rethrow.throwSupplier;
 
