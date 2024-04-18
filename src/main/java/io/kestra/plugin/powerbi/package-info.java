f1630482a381cd47b7dd33ae48784d23aa66b870
Loïc Mathieu <loikeseke@gmail.com>
2024-04-18 17:00:00 +0200

    feat: change plugin category

--- src/main/java/io/kestra/plugin/powerbi/package-info.java
+++ src/main/java/io/kestra/plugin/powerbi/package-info.java
@@ -1,6 +1,6 @@
 @PluginSubGroup(
     description = "This sub-group of plugins contains tasks for using Microsoft Power BI.",
-    categories = PluginSubGroup.PluginCategory.MISC
+    categories = PluginSubGroup.PluginCategory.BI
 )
 package io.kestra.plugin.powerbi;
 
