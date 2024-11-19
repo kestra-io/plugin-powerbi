18054865b9238e9b3bf77e23b9541d79b8b7f521
Loïc Mathieu <loikeseke@gmail.com>
2024-11-19 09:55:00 +0100

    fix: PowerBI refresh

--- src/main/java/io/kestra/plugin/powerbi/models/Refresh.java
+++ src/main/java/io/kestra/plugin/powerbi/models/Refresh.java
@@ -1,17 +1,15 @@
 package io.kestra.plugin.powerbi.models;
 
-import lombok.Getter;
 import lombok.Value;
-import lombok.extern.jackson.Jacksonized;
 
 import java.time.Instant;
 
-@Jacksonized
 @Value
 public class Refresh {
     String requestId;
-    String id;
     String refreshType;
     Instant startTime;
+    Instant endTime;
     String status;
+    String extendedStatus;
 }
